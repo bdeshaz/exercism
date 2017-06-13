b51752a20fd457fdf43af3cbe2fc88b6ef4b1da8
bdeshaz <bdeshaz@gmail.com>
2017-06-13 10:11:15 -0400

    fixed indentation error

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -2,10 +2,9 @@ from re import sub
 
 def is_pangram(words):
     """
-        Thanks to DUznanki for help on solution
-        I explored re.sub and regex format due to their solution.
-        As well, realized a conditional can be returned
+    Thanks to DUznanki for help on solution
+    I explored re.sub and regex format due to their solution.
+    As well, realized a conditional can be returned
     """
-
-	letters = set(sub('[^a-z]','',words.lower()))
-	return len(letters) == 26
+    letters = set(sub('[^a-z]','', words.lower()))
+    return len(letters) == 26
